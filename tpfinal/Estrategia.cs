e9e242f8938f733fa7f210aa7e461c0c7cec27f5
solmsl <mejiasilesol@gmail.com>
2026-05-27 19:11:12 -0300

    intento de implementacion del metodo buscarConOtro()

--- tpfinal/Estrategia.cs
+++ tpfinal/Estrategia.cs
@@ -39,7 +39,45 @@ namespace tpfinal
 
         public void BuscarConOtro(List<string> datos, int cantidad, List<Dato> collected)
         {
-            //Implementar
+            Dictionary<string, int> contador = new Dictionary<string, int>();
+
+            foreach (string texto in datos)
+            {
+                if (contador.ContainsKey(texto)) // si existe la ocurrencia por la clave 
+                {
+                    contador[texto]++; //suma esa ocurrencia al contador
+                }
+                else
+                {
+                    contador[texto] = 1; //sino le asigna un 1
+                }
+            }
+
+            List<Dato> listaDatos = new List<Dato>(); // crea lista de datos con la estructura Dato
+
+            foreach (var item in contador)
+            {
+                listaDatos.Add(new Dato(item.Value, item.Key)); //agrega el valor y la clave del dato en la lista de datos
+            }
+
+            //bubble sort descendente
+            for (int i = 0; i < listaDatos.Count - 1; i++) // recorre la lista menos el ultimo elemento que ya está ordenado
+            {
+                for (int j = 0; j < listaDatos.Count - i - 1; j++) // resta la vuelta que ya dió y la ultima que ya queda ordenada para repetir vueltas innecesarias
+                {
+                    if (listaDatos[j].ocurrencia < listaDatos[j + 1].ocurrencia) //compara entre un numero de ocurrencia y el de al lado
+                    { //intercambio de posiciones
+                        Dato aux = listaDatos[j]; //guarda temporalmente en aux el primer dato de comparacion de la izquierda
+                        listaDatos[j] = listaDatos[j + 1]; //asigna la ocurrencia de la derecha en la posicion del dato de la izquierda
+                        listaDatos[j + 1] = aux; // asigna la ocurrencia guardada en aux a la posicion de la derecha
+                    }
+                }
+            }
+
+            for (int i = 0; i < cantidad && i < listaDatos.Count; i++) // agrega la lista de datos en la lista collected recorriendo entre la cantidad de resultados elegidos por el usuario y la cantidad de elementos de la lista de datos
+            {
+                collected.Add(listaDatos[i]);
+            }
         }
 
 
--- tpfinal/obj/Debug/net6.0-windows/tpfinal.AssemblyInfo.cs
+++ tpfinal/obj/Debug/net6.0-windows/tpfinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpfinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0f55c132ffcc4410ff6696230273e2d5f6f241")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpfinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpfinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
